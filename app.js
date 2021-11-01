c4360472646be11eeb74a4848d8fb1492dcda7b9
Khailas R <khailas303@gmail.com>
2021-11-02 03:18:59 +0530

    updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017';
 
 const mongoOptions = {
@@ -12,48 +13,59 @@ MongoClient.connect(url, mongoOptions,
             return console.log(err);
         }
         
-        console.log(`DB connected: ${url}`);
-        const db = client.db('Register');   // to select a mongodb db
+        else {
 
-        const names = db.collection('names');   // create and get collection
-        
-        // insert into an existing document
-        names.insertOne({ name: 'Bruce Wayne' }, (err, result) => {});  
-        
-        // inserts multiple docs at once
-        names.insertMany([
-            { name: 'Thomas Wayne'},
-            { name: 'Alfred Pennyworth' },
-            { name: 'Martha Wayne' }
-        ], (err, results) => {});
-        
-        // to find all docs
-        names.find().toArray((err, results) => {
-            console.log(names);
-        });   
-        
-        // to find a specific doc
-        names.find({ name: 'Bruce Wayne '}).toArray((err, result) => {
-            console.log(result);
-        });
+            console.log(`DB connected: ${url}`);
+            const db = client.db('register');   // to select a mongodb db
+    
+            const names = db.collection('names');   // create and get collection
+            
+            // insert into an existing document
+            names.insertOne({ name: 'Bruce Wayne' }, (err, result) => {});  
+            
+            // inserts multiple docs at once
+            names.insertMany([
+                { name: 'Thomas Wayne'},
+                { name: 'Alfred Pennyworth' },
+                { name: 'Martha Wayne' }
+            ], (err, results) => {});
+            
+            // to find all docs
+            names.find().toArray((err, results) => {
+                console.log(names);
+            });   
+            
+            // to find a specific doc
+            names.find({ name: 'Bruce Wayne '}).toArray((err, result) => {
+                console.log(result);
+            });
+    
+            // gets the topmost doc matches the filter
+            // names.findOne({ name: 'Bruce Wayne' }, (err, result) => {
+            //     console.log(result);
+            // });
+            const find = async () => {
+                try {
+                    return await names.findOne({ name: 'Martha Wayne'});
+                }
+                catch (err) {
+                    console.log(err);
+                }
+            };
 
-        // gets the topmost doc matches the filter
-        names.findOne({ name: 'Bruce Wayne' }, (err, result) => {
-            console.log(result);
-        });
+    
+            // updating an existing doc
+            names.updateOne({ name : 'Thomas Wayne' }, {
+                $set: { name: 'Jim Gordon' },
+            }, (err, result) => {
+                console.log(result);
+            });
+    
+            // del doc
+            names.deleteOne({ name: 'Jim Gordon' }, (err, result) => {
+                console.log(result);
+            });
 
-        // updating an existing doc
-        names.updateOne({ name : 'Thomas Wayne' }, {
-            $set: { name: 'Jim Gordon' },
-        }, (err, result) => {
-            console.log(result);
-        });
-
-        // del doc
-        names.deleteOne({ name: 'Jim Gordon' }, (err, result) => {
-            console.log(result);
-        });
-
-        client.close()      // closing connection
+            client.close()      // closing connection
+        }
 });        
-
